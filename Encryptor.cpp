32fe49fd0d46e52e4fc8be2937778e5c89612bdb
Fhelipx <36421383+Fhelipx@users.noreply.github.com>
2022-04-10 12:01:58 -0300

    Update Encryptor.cpp

--- Encryptor.cpp
+++ Encryptor.cpp
@@ -2,10 +2,9 @@
 
 std::string BEncryptor::encryptor(std::string text)
 {
-
 	for (int i = 0; i < text.length(); i++)
 	{
-		//Goes back 1 Char from the ASCII
+		//Goes back 1 char from the ASCII
 		text[i] = text[i] - 1;
 	}
 
@@ -14,12 +13,11 @@ std::string BEncryptor::encryptor(std::string text)
 
 std::string BEncryptor::decryptor(std::string text)
 {
-
 	for (int i = 0; i < text.length(); i++)
 	{
-		//Goes up 1 Char from the ASCII
+		//Goes up 1 char from the ASCII
 		text[i] = text[i] + 1;
 	}
-
+	
 	return text;
 }
